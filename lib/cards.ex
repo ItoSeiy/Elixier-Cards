5156e160b8896e145fbcc19432195e6b62055ec2
伊東聖矢 <itoseiya1219@gmail.com>
2022-07-30 17:21:07 +0900

    create [create deck] function

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,14 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
 
-  @doc """
-  Hello world.
+  @spec create_deck :: [<<_::40, _::_*8>>, ...]
+  def create_deck() do
+    numbers = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+      "Jack", "Queen", "King"]
 
-  ## Examples
+    suits = ["Spade", "Clover", "Diamond", "Heart"]
 
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    for s <- suits, n <- numbers do
+      s <> n
+    end
   end
 end
