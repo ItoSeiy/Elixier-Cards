ec748e57c9ac4c01acac7215626a29d23ee39295
伊東聖矢 <itoseiya1219@gmail.com>
2022-07-30 18:07:42 +0900

    デッキシャッフル関数 捜索関数　実装

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,14 +1,33 @@
 defmodule Cards do
 
-  @spec create_deck :: [<<_::40, _::_*8>>, ...]
   def create_deck() do
     numbers = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
       "Jack", "Queen", "King"]
 
     suits = ["Spade", "Clover", "Diamond", "Heart"]
 
-    for s <- suits, n <- numbers do
-      s <> n
+    IO.inspect("==========CreateDeck==========")
+
+    deck = for suit <- suits, num <- numbers do
+      IO.inspect("#{suit}#{num}")
     end
+
+    shuffle(deck)
   end
+
+  defp shuffle(deck) do
+    IO.inspect("==========Shuffle==========")
+
+    for shffled_deck <- Enum.shuffle(deck) do
+      IO.inspect(shffled_deck)
+    end
+
+    contains?(deck, "SpadeAce")
+  end
+
+  defp contains?(deck, str) do
+    IO.inspect("==========Contains==========")
+    Enum.member?(deck, str)
+  end
+
 end
